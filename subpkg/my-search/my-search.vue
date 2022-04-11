828e9155b70ea2375b22538af7d201e327c6e829
wuyafeiya <wuyafeiya@gmail.com>
2022-04-11 21:54:39 +0800

    '2'

---
+++ subpkg/my-search/my-search.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count:1,
+				name:wj
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
